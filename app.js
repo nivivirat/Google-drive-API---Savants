f6b367f45d3ad9680932ab304deaf7309aa1370d
nivivirat <imniveditha1188@gmail.com>
2024-05-26 23:06:47 +0530

    chore: Update app.js to handle preflight requests and improve CORS headers

--- app.js
+++ app.js
@@ -7,20 +7,9 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-app.use(cors());
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
-    // Handle preflight requests
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
+app.use(cors({
+    origin: 'http://localhost:5173' // Replace this with your frontend's origin
+  }));  
 
 const IMAGES_DIR = '/tmp/images'; // Store images in /tmp directory for AWS Lambda
 
