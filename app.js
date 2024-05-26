0710df4711878329b15d79b4cf6f04948316a6f3
nivivirat <imniveditha1188@gmail.com>
2024-05-26 22:59:11 +0530

    chore: Add CORS headers to app.js

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const path = require('path');
 const app = express();
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 const IMAGES_DIR = '/tmp/images'; // Store images in /tmp directory for AWS Lambda
 
 // Create the images directory if it doesn't exist
